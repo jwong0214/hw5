2916187f81449bdf03d3cd3741a4d5427d34a811
Janessa Techathamawong <techatha@usc.edu>
2025-12-02 10:02:29 +0000

    HW5 submission stuff

--- schedwork.cpp
+++ schedwork.cpp
@@ -19,12 +19,11 @@ using namespace std;
 // Feel free to not use or delete.
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
-
 // Add prototypes for any helper functions here
-
+// sched[nDays][dailyNeed]
+bool backtrack (const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, vector<size_t>& shiftsUsed, size_t slotIndex);
 
 // Add your implementation of schedule() and other helper functions here
-
 bool schedule(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
@@ -37,9 +36,58 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    // sched n x d vector of vectors with d workers scheduled to work on each n days
+    // avail n x k vector of vectors with avaliability of k workers for each n days
+    // prepare output matrix nDays x dailyNeed
+    size_t nDays = avail.size();
+    size_t kWorkers = avail[0].size();
+    sched.resize(nDays, vector<Worker_T>(dailyNeed, INVALID_ID));
 
+    // track number of shifts assigned to each worker
+    vector<size_t> shiftsUsed(kWorkers, 0);
 
-
-
+    // start backtracking at slot 0 (day=0, position=0)
+    return backtrack(avail, dailyNeed, maxShifts, sched, shiftsUsed, 0);
 }
 
+bool backtrack(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, vector<size_t>& shiftsUsed, size_t slotIndex) {
+    size_t nDays = avail.size();
+    size_t kWorkers = avail[0].size();
+    size_t totalSlots = nDays * dailyNeed;
+
+    // base case: when positions in schedule have been filled
+    if (slotIndex == totalSlots) {
+        return true;
+    }
+
+    // convert matrix into flat indexes of the day and position of that day
+    size_t day = slotIndex / dailyNeed;
+    size_t pos = slotIndex % dailyNeed;
+
+    // try assigning each worker (DFS)
+    for (size_t w = 0; w < kWorkers; w++) {
+        // pruning (kinda)
+        // worker is avaliable on this day + shifts worked doesn't exceed max shifts
+        if ((!avail[day][w]) || (shiftsUsed[w] >= maxShifts)) {
+            continue;
+        }
+        // worker is not already scheduled on the day
+        if (find(sched[day].begin(), sched[day].end(), w) != sched[day].end()) {
+            continue;
+        }
+        // if passes prev if statements, then choose worker w
+        sched[day][pos] = w;
+        shiftsUsed[w]++;
+        
+        // recurse to the next slot
+        if (backtrack(avail, dailyNeed, maxShifts, sched, shiftsUsed, slotIndex+1)) {
+            return true; // found solution
+        }
+
+        // if there's no further solution when worker w is assigned to the day, backtrack
+        sched[day][pos] = INVALID_ID;
+        shiftsUsed[w]--;
+    }
+
+    return false; // no workers AT ALL fit so we FAIL
+}
--- wordle.cpp
+++ wordle.cpp
@@ -1,7 +1,7 @@
 #ifndef RECCHECK
 // For debugging
 #include <iostream>
-// For std::remove
+// For remove
 #include <algorithm> 
 #include <map>
 #include <set>
@@ -13,16 +13,115 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void recurse(string& current, const string& fixed, string floating, const set<string>& dict, set<string>& results, int index); // generate all possible valid strings
+bool validPrefix(const string& current, const set<string>& dict, int index);
 
 // Definition of primary wordle function
-std::set<std::string> wordle(
-    const std::string& in,
-    const std::string& floating,
-    const std::set<std::string>& dict)
+// creates a results set, creates a working string equal to in
+// calls recursive helper starting at index 0, returns results
+set<string> wordle(
+    const string& in,
+    const string& floating,
+    const set<string>& dict)
 {
     // Add your code here
+    set<string> results;
+    string current = in;
+    recurse(current, in, floating, dict, results, 0);
 
+    return results;
 }
 
 // Define any helper functions here
+
+// fixed letters stay fixed, floating letters appear at least once
+// remaining slots are any letters a-z, and word is valid (in dict)
+// constantly update floating string for pruning purposes
+void recurse(string& current, const string& fixed, string floating, const set<string>& dict, set<string>& results, int index) {
+    // pruning
+    // if we have fewer slots left than floating letters that need to appear
+    // potential word is INVALID
+    int remainingSlots = (int)current.size() - index;
+    if (remainingSlots < (int)floating.size()) {
+        return;
+    }
+
+    // base case, if the current index equal to size, we've filled all positions + word is complete
+    if (index == (int)current.size()) {
+        if ((floating.empty()) && (dict.find(current) != dict.end())) { // check dictionary
+            results.insert(current);
+        }
+        return;
+    }
+
+    // fixed slot (green)
+    if (fixed[index] != '-') {
+        char letter = fixed[index];
+        // make copies of the floating letters
+        // (seperate branches, seperate ways we deal with the floating letters)
+        string nextFloating = floating;
+        // was fixed letter listed as a floating letter?
+        size_t isFloating = nextFloating.find(letter);
+        if (isFloating != string::npos) {
+            nextFloating.erase(isFloating, 1);
+        }
+        // place fixed letter
+        char old = current[index];
+        current[index] = letter;
+        recurse(current, fixed, nextFloating, dict, results, index+1);
+        current[index] = old;
+        return; // only one branch
+    }
+
+    // floating letter case: if remaining slots equal floating string size,
+    // ALL remaining slots MUST be filled w floating letters
+    // for instructor3 case that takes five billion years
+    if (remainingSlots == (int)floating.size()) {
+        for (size_t i = 0; i < floating.size(); i++) {
+            char letter = floating[i];
+            string nextFloating = floating;
+            nextFloating.erase(i, 1);
+            char old = current[index];
+            current[index] = letter;
+            recurse(current, fixed, nextFloating, dict, results, index+1);
+            current[index] = old; // backtracking undoes any modifications made
+        }
+        return; // only one branch
+    }
+
+    // open slot (gray)
+    // try all 26 letters
+    for (char letter = 'a'; letter <= 'z'; letter++) {
+        string nextFloating = floating;
+        // did we place a floating letter?
+        size_t isFloating = nextFloating.find(letter);
+        if (isFloating != string::npos) {
+            nextFloating.erase(isFloating, 1);
+        }
+        // pruning
+        int nextRemaining = current.size() - (index + 1);
+        if (nextRemaining < (int)nextFloating.size()) {
+            continue;  // impossible, skip early
+        }
+        char old = current[index];
+        current[index] = letter;
+        // prefix pruning
+        // after placing letter, check if partial word
+        // is a prefix for a REAL dict word -> if not, prune
+        if (!validPrefix(current, dict, index+1)) {
+            current[index] = old;
+            continue; // skip branch
+        }
+        recurse(current, fixed, nextFloating, dict, results, index+1);
+        current[index] = old;
+    }
+}
+
+bool validPrefix(const string& current, const set<string>& dict, int index) {
+    string prefix = current.substr(0, index); // get prefix
+    auto it = dict.lower_bound(prefix); // find first dictionary word with prefix
+    if (it == dict.end()) { // no word has prefix
+        return false;
+    }
+    return it->compare(0, prefix.size(), prefix) == 0;
+}
